554ecf1719db56e5902221a5212d76b83dfe9ffb
Artem Fomin <artem.fomin@visma.com>
2024-03-20 22:51:30 +0200

    Add project files.

---
+++ Visma.Hackathon.SecurityAutomation.Web/ML/Models/InjectionDetection/InjectionDetectionModel.consumption.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace Visma.Hackathon.SecurityAutomation.Web.ML.Models
+{
+    public partial class InjectionDetectionModel
+    {
+        #region model input class
+        public class ModelInput
+        {
+            [LoadColumn(0)]
+            [ColumnName(@"Query")]
+            public string Query { get; set; }
+
+            [LoadColumn(1)]
+            [ColumnName(@"Label")]
+            public string Label { get; set; }
+
+        }
+
+        #endregion
+        
+        #region model output class
+        public class ModelOutput
+        {
+            [ColumnName(@"Query")]
+            public float[] Query { get; set; }
+
+            [ColumnName(@"Label")]
+            public uint Label { get; set; }
+
+            [ColumnName(@"Features")]
+            public float[] Features { get; set; }
+
+            [ColumnName(@"PredictedLabel")]
+            public string PredictedLabel { get; set; }
+
+            [ColumnName(@"Score")]
+            public float[] Score { get; set; }
+
+        }
+
+        #endregion
+
+        private static string MLNetModelPath = Path.GetFullPath(".\\ML\\Models\\InjectionDetection\\InjectionDetectionModel.mlnet");
+
+        public static readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(() => CreatePredictEngine(), true);
+
+
+        private static PredictionEngine<ModelInput, ModelOutput> CreatePredictEngine()
+        {
+            var mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load(MLNetModelPath, out var _);
+            return mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+        }
+        
+        public static IOrderedEnumerable<KeyValuePair<string, float>> PredictAllLabels(ModelInput input)
+        {
+            var predEngine = PredictEngine.Value;
+            var result = predEngine.Predict(input);
+            return GetSortedScoresWithLabels(result);
+        }
+
+        public static IOrderedEnumerable<KeyValuePair<string, float>> GetSortedScoresWithLabels(ModelOutput result)
+        {
+            var unlabeledScores = result.Score;
+            var labelNames = GetLabels(result);
+
+            Dictionary<string, float> labledScores = new Dictionary<string, float>();
+            for (int i = 0; i < labelNames.Count(); i++)
+            {
+                // Map the names to the predicted result score array
+                var labelName = labelNames.ElementAt(i);
+                labledScores.Add(labelName.ToString(), unlabeledScores[i]);
+            }
+
+            return labledScores.OrderByDescending(c => c.Value);
+        }
+
+        private static IEnumerable<string> GetLabels(ModelOutput result)
+        {
+            var schema = PredictEngine.Value.OutputSchema;
+
+            var labelColumn = schema.GetColumnOrNull("Label");
+            if (labelColumn == null)
+            {
+                throw new Exception("Label column not found. Make sure the name searched for matches the name in the schema.");
+            }
+
+            var keyNames = new VBuffer<ReadOnlyMemory<char>>();
+            labelColumn.Value.GetKeyValues(ref keyNames);
+            return keyNames.DenseValues().Select(x => x.ToString());
+        }
+
+        public static ModelOutput Predict(ModelInput input)
+        {
+            var predEngine = PredictEngine.Value;
+            return predEngine.Predict(input);
+        }
+
+    }
+}
---
+++ Visma.Hackathon.SecurityAutomation.Web/ML/Models/InjectionDetection/InjectionDetectionModel.training.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Trainers.FastTree;
+
+namespace Visma.Hackathon.SecurityAutomation.Web.ML.Models
+{
+    public partial class InjectionDetectionModel
+    {
+        public const string RetrainFilePath =  @".\..\TrainingData\SQLInjectionDetection\Modified_SQL_Dataset.csv";
+        public const char RetrainSeparatorChar = ',';
+        public const bool RetrainHasHeader =  true;
+
+        public static void Train(string outputModelPath, string inputDataFilePath = RetrainFilePath, char separatorChar = RetrainSeparatorChar, bool hasHeader = RetrainHasHeader)
+        {
+            var mlContext = new MLContext();
+
+            var data = LoadIDataViewFromFile(mlContext, inputDataFilePath, separatorChar, hasHeader);
+            var model = RetrainModel(mlContext, data);
+            SaveModel(mlContext, model, data, outputModelPath);
+        }
+
+        public static IDataView LoadIDataViewFromFile(MLContext mlContext, string inputDataFilePath, char separatorChar, bool hasHeader)
+        {
+            return mlContext.Data.LoadFromTextFile<InjectionDetectionModel.ModelInput>(inputDataFilePath, separatorChar, hasHeader);
+        }
+        
+        public static void SaveModel(MLContext mlContext, ITransformer model, IDataView data, string modelSavePath)
+        {
+            DataViewSchema dataViewSchema = data.Schema;
+
+            using (var fs = File.Create(modelSavePath))
+            {
+                mlContext.Model.Save(model, dataViewSchema, fs);
+            }
+        }
+
+        public static ITransformer RetrainModel(MLContext mlContext, IDataView trainData)
+        {
+            var pipeline = BuildPipeline(mlContext);
+            var model = pipeline.Fit(trainData);
+
+            return model;
+        }
+
+        public static IEstimator<ITransformer> BuildPipeline(MLContext mlContext)
+        {
+            var pipeline = mlContext.Transforms.Text.FeaturizeText(inputColumnName:@"Query",outputColumnName:@"Query")      
+                                    .Append(mlContext.Transforms.Concatenate(@"Features", new []{@"Query"}))      
+                                    .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName:@"Label",inputColumnName:@"Label",addKeyValueAnnotationsAsText:false))      
+                                    .Append(mlContext.MulticlassClassification.Trainers.OneVersusAll(binaryEstimator:mlContext.BinaryClassification.Trainers.FastTree(new FastTreeBinaryTrainer.Options(){NumberOfLeaves=473,MinimumExampleCountPerLeaf=2,NumberOfTrees=77,MaximumBinCountPerFeature=39,FeatureFraction=0.242104163857099,LearningRate=0.999999776672986,LabelColumnName=@"Label",FeatureColumnName=@"Features"}),labelColumnName: @"Label"))      
+                                    .Append(mlContext.Transforms.Conversion.MapKeyToValue(outputColumnName:@"PredictedLabel",inputColumnName:@"PredictedLabel"));
+
+            return pipeline;
+        }
+    }
+ }
